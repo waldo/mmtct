4b6a93550c968efc44045c54881d61a6ffd41eef
Waldo <ben.walsham@meltingwaldo.com>
2012-01-20 21:14:53 +0000

    Rails 3.2 new install.
    - Removing test.
    - Removed docs/ and readme files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mmtct::Application.config.secret_token = 'ac892530ea7c37098e622e9245ce87851b3fbaeb55dc148c5dd0312ebbb428fad99bca579b5bf0143ead8aa59d03792e7a9e6797609251d20e830a206b360b65'
