75cc91c0a4f0f45337b1e326ad4f266735b0598b
Waldo <ben.walsham@meltingwaldo.com>
2012-01-21 00:45:50 +0000

    Named routes.
    - Name the custom tin and holder routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Mmtct::Application.routes.draw do
   root :to => "static#index"
 
-  match ":tin_code" => "tins#show"
-  match ":tin_code/holders/:id" => "holders#update", :via => :put
-  match ":tin_code/holders" => "holders#create", :via => :post
+  match ":tin_code" => "tins#show", :as => "tin"
+  match ":tin_code/holders/:id" => "holders#update", :via => :put, :as => "holder"
+  match ":tin_code/holders" => "holders#create", :via => :post, :as => "holders"
 end
