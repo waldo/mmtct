0685e21cf878e9cebb075340b07fff7763c7f697
Waldo <ben.walsham@meltingwaldo.com>
2012-01-22 13:33:49 +0000

    Edit most recent history entry
    - jQuery hide and show last history entry and show a form to edit it.
    - Form can override existing image or will keep original image by
    default.
    - Can only edit within 24 hours of original posting.

--- app/assets/javascripts/tins.js.coffee
+++ app/assets/javascripts/tins.js.coffee
@@ -1,3 +1,7 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+jQuery ($) ->
+	$(".hide_me").hide()
+	$("a#edit_holder").click (event) ->
+		event.preventDefault()
+		$elem = $("div#holder_form")
+		$("##{$elem.data('id')}").hide();
+		$elem.fadeIn()
--- app/controllers/holders_controller.rb
+++ app/controllers/holders_controller.rb
@@ -6,23 +6,24 @@ class HoldersController < ApplicationController
     @holder.ip = request.remote_ip
 
     respond_to do |format|
-      if @holder.save
-        format.html { redirect_to tin_path(@holder.tin.code), notice: 'Mission accomplished - the tin must be passed on soon.' }
+      if @tin and @holder.save
+        format.html { redirect_to tin_path(@holder.tin.code), notice: "Mission accomplished - the tin must be passed on soon." }
       else
-        format.html { redirect_to tin_path(@holder.tin.code), alert: 'Eep, error.' }
+        format.html { redirect_to tin_path(@holder.tin.code), alert: "Eep, error." }
       end
     end
   end
 
-  # PUT /holders/1
+  # PUT /:tin_code/holders/1
   def update
-    @holder = Holder.find(params[:id])
+    tin = Tin.find_by_code(params[:tin_code])
+    @holder = Holder.where("tin_id = ? and id = ? and created_at >= ?", tin.id, params[:id], 1.day.ago).first
 
     respond_to do |format|
-      if @holder.update_attributes(params[:holder])
-        format.html { redirect_to tin_path(@holder.tin.code), notice: 'Holder was successfully updated.' }
+      if @holder and @holder.update_attributes(params[:holder])
+        format.html { redirect_to tin_path(@holder.tin.code), notice: "Your wish is my command." }
       else
-        format.html { render action: "edit" }
+        format.html { redirect_to root_path, notice: "Eep, error." }
       end
     end
   end
