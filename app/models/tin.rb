ef72a08df1dd9f928ad927da3a7e4ba922b42d41
Waldo <ben.walsham@meltingwaldo.com>
2012-01-24 23:54:39 +0000

    Give tins codes automatically on save

--- app/models/tin.rb
+++ app/models/tin.rb
@@ -1,3 +1,9 @@
 class Tin < ActiveRecord::Base
 	has_many :holders
+	before_create :set_code
+
+	def set_code
+		self[:code] ||= Digest::SHA1.hexdigest("1$#{rand(1_000_000_000)}").slice(0..7)
+	end
+	private :set_code
 end
